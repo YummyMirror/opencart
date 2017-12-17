c899711821e82a76f1348551dcec1990e63db2c2
YummyMirror <anatolianiukevich@gmail.com>
2017-12-17 20:30:11 +0300

    Have added 'validLoginDataJson' dataProvider for 'loginTest'

--- opencart/src/test/java/com/opencart/tests/PublicLoginTests.java
+++ opencart/src/test/java/com/opencart/tests/PublicLoginTests.java
@@ -1,17 +1,50 @@
 package com.opencart.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.opencart.models.PublicLoginData;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.Iterator;
+import java.util.List;
 import static org.testng.Assert.*;
 
 public class PublicLoginTests extends TestBase {
-    @Test
-    public void loginTest() {
+    @DataProvider
+    public Iterator<Object[]> validLoginDataJson() throws IOException {
+        File file = new File("src/test/resources/dataProviders/validLoginData.json");
+        FileReader reader = new FileReader(file);
+        BufferedReader buffReader = new BufferedReader(reader);
+        String line = buffReader.readLine();
+        String json = "";
+        while (line != null) {
+            json += line;
+            line = buffReader.readLine();
+        }
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<List<PublicLoginData>>(){}.getType();
+        List<PublicLoginData> list = gson.fromJson(json, collectionType);
+        reader.close();
+        buffReader.close();
+        return list.stream().map(l -> new Object[] {l}).iterator();
+    }
+
+    @BeforeMethod
+    public void precondition() {
+        app.getPublicNaviPage().clickMyAccount();
+        List<WebElement> menuItems = app.getPublicNaviPage().getMenuItems();
+        if (menuItems.size() > 2)
+            app.getPublicNaviPage().clickLogout();
+    }
+
+    @Test(enabled = true, dataProvider = "validLoginDataJson", priority = 1)
+    public void loginTest(PublicLoginData loginData) {
         app.getPublicNaviPage().clickMyAccount();
         app.getPublicNaviPage().clickLogin();
-
-        PublicLoginData loginData = new PublicLoginData().setEmail("")
-                                                         .setPassword("");
         app.getPublicLoginPage().fillLoginForm(loginData);
 
         assertEquals(app.getPublicLoginPage().getMyAccountTitle(), "My Account", "User isn't logged in!");
